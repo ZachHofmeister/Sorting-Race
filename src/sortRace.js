a560f9e56ca7ca0d076839d42f5807aeae94670d
ZachHofmeister <zachhof@me.com>
2021-03-09 17:21:47 -0800

    Added the ability to draw a character in each cell, and reorganized to be cleaner and ready for sorting algo implementation.

--- src/sortRace.js
+++ src/sortRace.js
@@ -8,13 +8,13 @@
 
 var g_canvas = { cell_size:14, wid:70, hgt:48 }; // JS Global var, w canvas size info.
 var g_frame_cnt = 0; // Setup a P5 display-frame counter, to do anim
-var g_frame_mod = 1; // Update every 'mod' frames.
+var g_frame_mod = 3; // Update every 'mod' frames.
 var g_stop = 0; // Go by default.
 
-var g_algoSS = {col:0, passNum:0, color:"red"}
-var g_algoGP = {col:18, passNum:0, color:"yellow"}
-var g_algoMS = {col:36, passNum:0, color:"green"}
-var g_algoQS = {col:54, passNum:0, color:"blue"}
+var g_algoSS = {col:0, passNum:0, color:"red", string:"0123456789ABCDEF"}
+var g_algoGP = {col:18, passNum:0, color:"yellow", string:"0123456789ABCDEF"}
+var g_algoMS = {col:36, passNum:0, color:"green", string:"0123456789ABCDEF"}
+var g_algoQS = {col:54, passNum:0, color:"blue", string:"0123456789ABCDEF"}
 
 var width;
 var height;
@@ -40,47 +40,63 @@ function draw() { // P5 Frame Re-draw Fcn, Called for Every Frame.
 
 function pass() {
 	//Selection Sort
-	var currentColor = color(random(0, 255), random(0,255), random(0,255));
-	for (var i = 0; i < 16; ++i) {
-		let x = g_algoSS.col + i;
-		let y = (g_algoSS.passNum % (g_canvas.hgt - 2)) + 2; //0 - 62
-		// console.log("SS: " + x + " " + y);
-		drawCell(x, y, currentColor);
-	}
+	drawString(g_algoSS);
+	g_algoSS.string = selecSortOnce(g_algoSS.string);
 	++g_algoSS.passNum;
 	//Gold's Pore Sort
-	currentColor = color(random(0, 255), random(0,255), random(0,255));
-	for (var i = 0; i < 16; ++i) {
-		let x = g_algoGP.col + i;
-		let y = (g_algoGP.passNum % (g_canvas.hgt - 2)) + 2; //1 - 63
-		drawCell(x, y, currentColor);
-	}
+	drawString(g_algoGP);
+	g_algoGP.string = goldsPoreOnce(g_algoGP.string);
 	++g_algoGP.passNum;
 	//Merge Sort
-	currentColor = color(random(0, 255), random(0,255), random(0,255));
-	for (var i = 0; i < 16; ++i) {
-		let x = g_algoMS.col + i;
-		let y = (g_algoMS.passNum % (g_canvas.hgt - 2)) + 2; //1 - 63
-		drawCell(x, y, currentColor);
-	}
+	drawString(g_algoMS);
+	g_algoMS.string = mergeSortOnce(g_algoMS.string);
 	++g_algoMS.passNum;
 	//Quick Sort
-	currentColor = color(random(0, 255), random(0,255), random(0,255));
-	for (var i = 0; i < 16; ++i) {
-		let x = g_algoQS.col + i;
-		let y = (g_algoQS.passNum % (g_canvas.hgt - 2)) + 2; //1 - 63
-		drawCell(x, y, currentColor);
-	}
+	drawString(g_algoQS);
+	g_algoQS.string = quickSortOnce(g_algoQS.string);
 	++g_algoQS.passNum;
 }
 
-function drawCell(x, y, color) {
+function selecSortOnce(str) { //One pass for the selection sort algorithm
+	return "0123456789ABCDEF"; //return the outcome from the pass
+
+	// Note: parseInt(char, 16) can be used to get the numerical value of a hex character '1'-'F' => 0-15
+}
+
+function goldsPoreOnce(str) { //One pass for the gold's pore sorting algorithm
+	return "0123456789ABCDEF";
+}
+
+function mergeSortOnce(str) { //One pass for the merge sort algorithm
+	return "0123456789ABCDEF";
+}
+
+function quickSortOnce(str) { //One pass for the quick sort algorithm
+	return "0123456789ABCDEF";
+}
+
+function drawString(algoObject) {
+	var currentColor = color(random(0, 255), random(0,255), random(0,255));
+	// var currentColor = "white";
+	for (var i = 0; i < 16; ++i) {
+		let x = algoObject.col + i;
+		let y = (algoObject.passNum % (g_canvas.hgt - 2)) + 2; //0 - 62
+		// console.log("SS: " + x + " " + y);
+		drawCell(x, y, currentColor, algoObject.string[i]);
+	}
+}
+
+function drawCell(x, y, color, char) {
 	let sz = g_canvas.cell_size;
     let rectX = 1+ x*sz; // Set x one pixel inside the sz-by-sz cell.
     let rectY = 1+ y*sz;
 
 	fill(color);
 	rect(rectX, rectY, sz, sz);
+	textSize(sz);
+	textAlign(CENTER, CENTER);
+	fill("#000");
+	text(char, rectX + (sz / 2), rectY + (sz / 2) + 2);
 }
 
 function keyPressed() {
